61a2d44946ec85d4351d2fd9312d16b482cd3205
champion0406 <barney0406@gmail.com>
2017-11-07 11:43:25 +0800

    Onclick

--- app/src/main/java/tom/com/bmi/MainActivity.java
+++ app/src/main/java/tom/com/bmi/MainActivity.java
@@ -5,15 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String s = new String("abc");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bHelp = (Button) findViewById(R.id.b_help);
+        bHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("xxx")
+                        .setMessage("BMI原來的設計是一個用於公眾健康研究的統計工具。當需要知道肥胖是否為某一疾病的致病原因時，可以把病人的身高及體重換算成BMI，再找出其數值及病發率是否有線性關連。")
+                        .setPositiveButton("OK",null)
+                        .show();
+            }
+        });
     }
     public void bmi(View view){
      //   System.out.println("what???");
@@ -24,9 +37,9 @@ public class MainActivity extends AppCompatActivity {
         float height = Float.parseFloat(edHeight.getText().toString());
         float bmi = weight/(height*height);
         new AlertDialog.Builder(this)
-                .setMessage("Your BMI is"+bmi)
-                .setTitle("my title")
-                .setPositiveButton("OK",null)
+                .setMessage(getString(R.string.your_bmi)+bmi)
+                .setTitle(R.string.my_title)
+                .setPositiveButton(R.string.ok,null)
                 .show();
 
 
